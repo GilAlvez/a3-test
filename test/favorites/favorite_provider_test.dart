5100521b99aea94772611db6ba028e5c49901160
Gil Alves <gildson_af@outlook.com>
2023-03-20 10:12:16 -0300

    feat: Adds a new test to fav provider

--- test/favorites/favorite_provider_test.dart
+++ test/favorites/favorite_provider_test.dart
@@ -6,7 +6,7 @@ void main() {
     late FavoritesProvider provider;
     setUp(() => provider = FavoritesProvider());
 
-    test('should init empty', () {
+    test('favorites list should init empty', () {
       expect(provider.favorites, isEmpty);
     });
 
@@ -22,7 +22,7 @@ void main() {
       expect(provider.favorites, hasLength(equals(0)));
     });
 
-    testWidgets('should removeFavorites remove items with id only', (WidgetTester tester) async {
+    testWidgets('removeFavorites should remove items with id only', (WidgetTester tester) async {
       provider.toggleFavorites(1, 'Test1', 'https://example.com');
       provider.toggleFavorites(2, 'Test2', 'https://example.com');
 
@@ -32,6 +32,18 @@ void main() {
       provider.removeFavorites(2);
 
       expect(provider.favorites, hasLength(equals(0)));
+    });
+
+    testWidgets('removeFavorites should not remove if id doesnt exist', (WidgetTester tester) async {
+      provider.toggleFavorites(1, 'Test1', 'https://example.com');
+      provider.toggleFavorites(2, 'Test2', 'https://example.com');
+
+      expect(provider.favorites, hasLength(equals(2)));
+
+      provider.removeFavorites(3);
+      provider.removeFavorites(4);
+
+      expect(provider.favorites, hasLength(equals(2)));
     });
   });
 }
